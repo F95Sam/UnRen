95afbc20c18466cfc56922bb943a243e6c5360bf
madeddy <mad-eddy@gmx.de>
2020-01-29 06:30:23 +0100

    Some improvements (#2)
    - Checks added:
      - Warning if the script lies not in the game root and loc. unknown.
      - Warning if the script IS in the "game" dir.
      - Warnings if rpatool or unrpyc after unpacking not there.
    - Aranged init checks in more logical order. (script/root > py > powersh.)
    - Added rpi format to the archive search.
    
    * Jump to cleanup processes on error
    
    * Resolved #issue-526387668, some restructuring
    - removed unnecessary warning
    - path checks more sturdy
    - made sure if tools are missing the cleanup happens
    - changed error msg slighly

--- UnRen.bat
+++ UnRen.bat
@@ -1,17 +1,18 @@
 @echo off
 REM [DEV//WARN_START
 title UnRen.bat Error
-echo.
+echo/
 echo   Error!
-echo.
-echo   This is the development version of UnRen. This is non functional and not intended to be used by end users.
+echo/
+echo   This is the development version of UnRen. This is non functional and not
+echo   intended to be used by end users.
 echo   Please use the release version instead, found in the F95zone thread:
-echo.
+echo/
 echo   https://f95zone.to/threads/unren-bat-rpa-extractor-rpyc-decompiler-console-developer-menu-enabler.3083/
-echo.
+echo/
 pause>nul|set/p=.  Press any key to exit...
-echo.
-pause 
+echo/
+pause
 exit
 REM DEV//WARN_END]
 REM --------------------------------------------------------------------------------
@@ -43,61 +44,72 @@ REM set rpatool01=
 REM --------------------------------------------------------------------------------
 REM !! DO NOT EDIT BELOW THIS LINE !!
 REM --------------------------------------------------------------------------------
-set "version=0.9-dev ([DEV//BUILD_DATE])"
+set "version=0.9.0-dev ([DEV//BUILD_DATE])"
 title UnRen.bat v%version%
 :init
 REM --------------------------------------------------------------------------------
 REM Splash screen
 REM --------------------------------------------------------------------------------
 cls
-echo.
+echo/
 echo     __  __      ____               __          __
 echo    / / / /___  / __ \___  ____    / /_  ____ _/ /_
 echo   / / / / __ \/ /_/ / _ \/ __ \  / __ \/ __ ^`/ __/
 echo  / /_/ / / / / _^, _/  __/ / / / / /_/ / /_/ / /_
 echo  \____/_/ /_/_/ ^|_^|\___/_/ /_(_)_.___/\__^,_/\__/ v%version%
 echo   Sam @ www.f95zone.to
-echo.
+echo/
 echo  ----------------------------------------------------
-echo.
+echo/
 
 REM --------------------------------------------------------------------------------
-REM We need powershell for later, make sure it exists
+REM Set up the work paths and assert script, python and powershell location
+REM Note: 'if/else/else if' would be nice, but make just problems in batch
 REM --------------------------------------------------------------------------------
-if not exist "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" (
-	echo    ! Error: Powershell is required, unable to continue.
-	echo             This is included in Windows 7, 8, 10. XP/Vista users can
-	echo             download it here: http://support.microsoft.com/kb/968929
-	echo.
-	pause>nul|set/p=.            Press any key to exit...
-	exit
-)
+set err=0
+set err_msg="Unknown reason..."
+set currentdir=%~dp0
 
-REM --------------------------------------------------------------------------------
-REM Set our paths, and make sure we can find python exe
-REM --------------------------------------------------------------------------------
-set "currentdir=%~dp0%"
-set "pythondir=%currentdir%..\lib\windows-i686\"
-set "renpydir=%currentdir%..\renpy\"
-set "gamedir=%currentdir%"
 if exist "game" if exist "lib" if exist "renpy" (
 	set "pythondir=%currentdir%lib\windows-i686\"
 	set "renpydir=%currentdir%renpy\"
 	set "gamedir=%currentdir%game\"
+	goto :path_ok
 )
 
+if exist "..\game" if exist "..\lib" if exist "..\renpy" (
+	set "pythondir=%currentdir%..\lib\windows-i686\"
+	set "renpydir=%currentdir%..\renpy\"
+	set gamedir=%currentdir%
+	goto :path_ok
+)
+
+echo    ! Error: The location of UnRen appears to be wrong. It should
+echo             be in the game's root directory.
+echo             (dirs 'game', 'lib', 'renpy' are present)
+echo/
+goto :error
+
+:path_ok
 if not exist "%pythondir%python.exe" (
 	echo    ! Error: Cannot locate python.exe, unable to continue.
 	echo             Are you sure we're in the game's root directory?
-	echo.
-	pause>nul|set/p=.            Press any key to exit...
-	exit
+	echo/
+	goto :error
 )
 
-:menu
+if not exist "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" (
+	echo    ! Error: Powershell is required, unable to continue.
+	echo             This is included in Windows 7, 8, 10. XP/Vista users can
+	echo             download it here: http://support.microsoft.com/kb/968929
+	echo/
+	goto :error
+)
+
 REM --------------------------------------------------------------------------------
 REM Menu selection
 REM --------------------------------------------------------------------------------
+:menu
 set exitoption=
 echo   Available Options:
 echo     1) Extract RPA packages
@@ -106,27 +118,28 @@ echo     3) Enable Console and Developer Menu
 echo     4) Enable Quick Save and Quick Load
 echo     5) Force enable skipping of unseen content
 echo     6) Force enable rollback (scroll wheel)
-echo.
+echo/
 echo     9) All of the above
-echo.
+echo/
 set /p option=.  Enter a number: 
-echo.
+echo/
 echo  ----------------------------------------------------
-echo.
-if "%option%"=="1" goto extract
-if "%option%"=="2" goto decompile
-if "%option%"=="3" goto console
-if "%option%"=="4" goto quick
-if "%option%"=="5" goto skip
-if "%option%"=="6" goto rollback
-if "%option%"=="9" goto extract
-goto init
+echo/
+if "%option%"=="1" goto :extract
+if "%option%"=="2" goto :decompile
+if "%option%"=="3" goto :console
+if "%option%"=="4" goto :quick
+if "%option%"=="5" goto :skip
+if "%option%"=="6" goto :rollback
+if "%option%"=="9" goto :extract
+goto :init
 
-:extract
 REM --------------------------------------------------------------------------------
 REM Write _rpatool.py from our base64 strings
 REM --------------------------------------------------------------------------------
+:extract
 set "rpatool=%currentdir%_rpatool.py"
+echo/
 echo   Creating rpatool...
 if exist "%rpatool%.tmp" (
 	del "%rpatool%.tmp"
@@ -143,38 +156,48 @@ set "rpatoolps=%rpatoolps:^=^^%"
 set "rpatoolps=%rpatoolps:&=^&%"
 powershell.exe -nologo -noprofile -noninteractive -command "& { [IO.File]::WriteAllBytes(\"%rpatoolps%\", [Convert]::FromBase64String([IO.File]::ReadAllText(\"%rpatoolps%.tmp\"))) }"
 
-echo.
+REM --------------------------------------------------------------------------------
+REM Check if rpatool is there.
+REM --------------------------------------------------------------------------------
+if not exist "%rpatool%" (
+	echo    ! Error: 'rpatool' is missing. Please check if UnRen and Powershell
+	echo             are working correctly.
+	echo/
+	set err=1 & goto :rpatool_cleanup
+)
 
 REM --------------------------------------------------------------------------------
 REM Unpack RPA
 REM --------------------------------------------------------------------------------
+echo/
 echo   Searching for RPA packages
 cd "%gamedir%"
-set "PYTHONPATH=%pythondir%Lib"
 for %%f in (*.rpa) do (
 	echo    + Unpacking "%%~nf%%~xf" - %%~zf bytes
 	"%pythondir%python.exe" -O "%rpatool%" -x "%%f"
 )
-echo.
 
 REM --------------------------------------------------------------------------------
 REM Clean up
 REM --------------------------------------------------------------------------------
+:rpatool_cleanup
+echo/
 echo   Cleaning up temporary files...
 del "%rpatool%.tmp"
 del "%rpatool%"
-echo.
-if not "%option%" == "9" (
-	goto finish
+echo/
 )
+if not %err% == 0 goto :error
+if not "%option%" == "9" goto :finish
 
-:decompile
 REM --------------------------------------------------------------------------------
 REM Write to temporary file first, then convert. Needed due to binary file
 REM --------------------------------------------------------------------------------
+:decompile
 set "unrpyccab=%gamedir%..\_unrpyc.cab"
 set "decompilerdir=%gamedir%..\decompiler"
 set "unrpycpy=%gamedir%..\unrpyc.py"
+echo/
 echo   Creating _unrpyc.cab...
 if exist "%unrpyccab%.tmp" (
 	del "%unrpyccab%.tmp"
@@ -197,47 +220,57 @@ set "unrpyccabps=%unrpyccabps:^=^^%"
 set "unrpyccabps=%unrpyccabps:&=^&%"
 powershell.exe -nologo -noprofile -noninteractive -command "& { [IO.File]::WriteAllBytes(\"%unrpyccabps%\", [Convert]::FromBase64String([IO.File]::ReadAllText(\"%unrpyccabps%.tmp\"))) }"
 
-echo.
-
 REM --------------------------------------------------------------------------------
 REM Once converted, extract the cab file. Needs to be a cab file due to expand.exe
 REM --------------------------------------------------------------------------------
+echo/
 echo   Extracting _unrpyc.cab...
 mkdir "%decompilerdir%"
 expand -F:* "%unrpyccab%" "%decompilerdir%" >nul
 move "%decompilerdir%\unrpyc.py" "%unrpycpy%" >nul
 
 REM --------------------------------------------------------------------------------
+REM Check if unrpyc is there
+REM --------------------------------------------------------------------------------
+if not exist "%unrpycpy%" (
+	echo    ! Error: 'unrpyc' is missing. Please check if UnRen and Powershell
+	echo              are working correctly.
+	echo/
+	set err=1 & goto :unrpyc_cleanup
+)
+
+REM --------------------------------------------------------------------------------
 REM Decompile rpyc files
 REM --------------------------------------------------------------------------------
+echo/
 echo   Searching for rpyc files...
 cd "%gamedir%"
-set "PYTHONPATH=%pythondir%Lib"
 for /r %%f in (*.rpyc) do (
 	if not %%~nf == un (
 		echo    + Decompiling "%%~nf%%~xf" - %%~zf bytes
 		"%pythondir%python.exe" -O "%unrpycpy%" "%%f"
 	)
 )
-echo.
 
 REM --------------------------------------------------------------------------------
 REM Clean up
 REM --------------------------------------------------------------------------------
+:unrpyc_cleanup
+echo/
 echo   Cleaning up temporary files...
 del "%unrpyccab%.tmp"
 del "%unrpyccab%"
 del "%unrpycpy%"
 rmdir /Q /S "%decompilerdir%"
-echo.
-if not "%option%" == "9" (
-	goto finish
-)
+
+if not %err% == 0 goto :error
+if not "%option%" == "9" goto :finish
 
 :console
 REM --------------------------------------------------------------------------------
 REM Drop our console/dev mode enabler into the game folder
 REM --------------------------------------------------------------------------------
+echo/
 echo   Creating Developer/Console file...
 set "consolefile=%gamedir%unren-dev.rpy"
 if exist "%consolefile%" (
@@ -250,17 +283,15 @@ echo   config.console = True>> "%consolefile%"
 
 echo    + Console: SHIFT+O
 echo    + Dev Menu: SHIFT+D
-echo.
 
-:consoleend
-if not "%option%" == "9" (
-	goto finish
-)
+if not "%option%" == "9" goto :finish
 
-:quick
+
 REM --------------------------------------------------------------------------------
 REM Drop our Quick Save/Load file into the game folder
 REM --------------------------------------------------------------------------------
+:quick
+echo/
 echo   Creating Quick Save/Quick Load file...
 set "quickfile=%gamedir%unren-quick.rpy"
 if exist "%quickfile%" (
@@ -279,16 +310,14 @@ echo     pass>> "%quickfile%"
 echo    Default hotkeys:
 echo    + Quick Save: F5
 echo    + Quick Load: F9
-echo.
 
-if not "%option%" == "9" (
-	goto finish
-)
+if not "%option%" == "9" goto :finish
 
-:skip
 REM --------------------------------------------------------------------------------
 REM Drop our skip file into the game folder
 REM --------------------------------------------------------------------------------
+:skip
+echo/
 echo   Creating skip file...
 set "skipfile=%gamedir%unren-skip.rpy"
 if exist "%skipfile%" (
@@ -302,16 +331,14 @@ echo   renpy.config.allow_skipping = True>> "%skipfile%"
 echo   renpy.config.fast_skipping = True>> "%skipfile%"
 
 echo    + You can now skip all text using TAB and CTRL keys
-echo.
 
-if not "%option%" == "9" (
-	goto finish
-)
+if not "%option%" == "9" goto :finish
 
-:rollback
 REM --------------------------------------------------------------------------------
 REM Drop our rollback file into the game folder
 REM --------------------------------------------------------------------------------
+:rollback
+echo/
 echo   Creating rollback file...
 set "rollbackfile=%gamedir%unren-rollback.rpy"
 if exist "%rollbackfile%" (
@@ -331,24 +358,31 @@ echo   except:>> "%rollbackfile%"
 echo     pass>> "%rollbackfile%"
 
 echo    + You can now rollback using the scrollwheel
-echo.
 
-if not "%option%" == "9" (
-	goto finish
-)
-
-:finish
 REM --------------------------------------------------------------------------------
 REM We are done
 REM --------------------------------------------------------------------------------
+:finish
 echo  ----------------------------------------------------
-echo.
+echo/
 echo    Finished!
-echo.
+echo/
 echo    Enter "1" to go back to the menu, or any other
 set /p exitoption=.   key to exit: 
-echo.
+echo/
 echo  ----------------------------------------------------
-echo.
+
 if "%exitoption%"=="1" goto menu
-exit
+exit 0
+
+REM --------------------------------------------------------------------------------
+REM Bad end
+REM --------------------------------------------------------------------------------
+:error
+echo/
+echo    Terminating.
+echo    If a reason was stated correct the problem, if not check path
+echo    and script for possible issues.
+echo/
+pause>nul|set/p=.            Press any key to exit...
+exit 1
