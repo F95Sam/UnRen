f9b861af5d7b19b5e97c8ab071066f6f6e5c0009
madeddy <mad-eddy@gmx.de>
2020-04-20 22:42:29 +0200

    Major refactoring (unfinished) - add build step for rpy snippeds - each placeholder with unique name - removed unneedet *nix shell code - added missing py files

--- unren_build.py
+++ unren_build.py
@@ -18,11 +18,11 @@ as string embedded in a prepaired placeholder location in the main script.
 # pylint: disable=c0103, c0301, w0511, w0611
 
 
-__title__ = 'UnRen streambuilder'
+__title__ = 'UnRen builder'
 __license__ = 'Apache-2'
 __author__ = 'madeddy'
 __status__ = 'Development'
-__version__ = '0.4.0-alpha'
+__version__ = '0.5.0-alpha'
 
 
 import os
@@ -32,29 +32,47 @@ import pickle
 import base64
 
 
-def embed_stream(target_s, embed_data):
-    """Embed's the datastream in the unren script."""
-    with pt(target_s).open('rb+') as ofi:
+# Support func; used by multiple parts
+def embed_data(dst_file, data, placeholder):
+    """Embed's the given data in a target script."""
+    # FIXME: Placeholder must be flexible if this func is used by multiple tasks
+    # use a variable instead...
+    with pt(dst_file).open('rb+') as ofi:
         file_data = ofi.read()
         ofi.seek(0)
         ofi.truncate()
-        ofi.write(file_data.replace(b"_placeholder", embed_data))
+        ofi.write(file_data.replace(b"_placeholder", data))
 
 
-def py_packer(target_py_s):
-    """ the py."""
-    with pt(target_py_s).open('rb') as ofi:
-        py_stream = ofi.read()
-    return py_stream
+# Part 3: Optional (just for the win cmd)
+def duplicate_file(dst_file, data):
+    """Writes a new file with given content."""
+    with pt(dst_file).open('wb') as ofi:
+        ofi.write(data)
 
 
-def py2shell(target_py_s, target_sh_s, target_cmd_s):
-    """Constructs the py stream and embeds it in the sh/cmd files."""
-    py_stream = py_packer(target_py_s)
-    embed_stream(target_sh_s, py_stream)
-    # embed_stream(target_cmd_s, py_stream)
+def get_filedata(src_file):
+    """Opens a given file and returns the content as bytes type."""
+    with pt(src_file).open('rb') as ofi:
+        file_data = ofi.read()
+    return file_data
 
 
+def py2cmd(embed_py2, embed_py3, base_cmd, dst_cmd2, dst_cmd3):
+    """Constructs the py stream and embeds it in the cmd file."""
+    placeholder = "batch_placeholder"
+    cmd_stream = get_filedata(base_cmd)
+
+    duplicate_file(dst_cmd2, cmd_stream)
+    py_stream = get_filedata(embed_py2)
+    embed_data(cmd_stream, py_stream, placeholder)
+
+    duplicate_file(dst_cmd3, cmd_stream)
+    py_stream = get_filedata(embed_py3)
+    embed_data(cmd_stream, py_stream, placeholder)
+
+
+# Part 2; pack tools to py
 def tools_packer(tools_pth, pth_lst):
     """Packs the tools to a pickled and encoded stream."""
     store = {}
@@ -70,14 +88,6 @@ def tools_packer(tools_pth, pth_lst):
     return stream
 
 
-def tool2py(tools_pth, target_py3_s, target_py2_s):
-    """Constructs the tools stream and embeds it in the py file."""
-    packlist = path_walker(tools_pth)
-    toolstream = tools_packer(tools_pth, packlist)
-    embed_stream(target_py3_s, toolstream)
-    # embed_stream(target_py2_s, toolstream)
-
-
 def path_walker(tools_pth):
     """Walks the tools directory and collects a list of py files."""
     tool_lst = []
@@ -88,30 +98,75 @@ def path_walker(tools_pth):
     return tool_lst
 
 
+def tool2py(tools_pth, dst_py2, dst_py3):
+    """Constructs the tools stream and embeds it in the py file."""
+    placeholder = "tool_placeholder"
+    packlist = path_walker(tools_pth)
+    toolstream = tools_packer(tools_pth, packlist)
+    embed_data(dst_py3, toolstream, placeholder)
+    # embed_data(dst_py2, toolstream, placeholder)
+
+
+# Part 1
+def get_rpy_cfg(src_rpy):
+    """Reads the RenPy cfg data from a rpy file."""
+    with pt(src_rpy).open('rb') as ofi:
+        lines = ofi.readlines()
+        cfg_txt = lines[4:]
+    return cfg_txt
+
+
+def embed_rpycfg(dst_py):
+    """Gets the cfg text from every listed file and embeds it in the target py."""
+    # console_placeholder = "console_placeholder"
+    # quick_placeholder = "quick_placeholder"
+    # rollback_placeholder = "rollback_placeholder"
+    # skip_placeholder = "skip_placeholder"
+
+    rpy_lib = {"dev_con.rpy": "console_placeholder",
+               "quick.rpy": "quick_placeholder",
+               "rollback.rpy": "rollback_placeholder",
+               "skip.rpy": "skip_placeholder"}
+    for rpy_file, placeholder in rpy_lib.items():
+        rpycfg_txt = get_rpy_cfg(rpy_file)
+        embed_data(dst_py, rpycfg_txt, placeholder)
+
+
+def rpycfg2py(src_py2, src_py3, embed_py2, embed_py3):
+    """Embeds the rpy cfg snippeds in the py files."""
+    embed_rpycfg(src_py2)
+    embed_rpycfg(src_py3)
+    embed_rpycfg(embed_py2)
+    embed_rpycfg(embed_py3)
+
+
 def ur_main():
     """This executes all program steps."""
-    # QUESTION: I think paths could be fixed; eleminates cli parsing but rigide
+    # TODO: Add argparser so we can choose wich part we want to execute
+    dst_py2 = "unren_py27.py"
+    dst_py3 = "unren_py36.py"
+    embed_py2 = "unren_py27_embed.py"
+    embed_py3 = "unren_py36_embed.py"
+    # Part 1
+    # if cfg.write_cfg:
+    rpycfg2py(dst_py2, dst_py3, embed_py2, embed_py3)
 
-    # Part 1 - embed tools in the py files  # hm...do we use a absolute path:
+    # Part 2 - embed tools in the py files  # hm...do we use a absolute path:
     # tools_pth = pt('/home/olli/Code/tst/ur_tools')
     # or relative path in a fixed dir struct:
+    # if cfg.make_py:
     tools_pth = pt('ur_tools')
-    if len(sys.argv) > 1:
-        target_py_s = sys.argv[1]
-    else:
-        target_py3_s = "unren_py36.py"
-        target_py2_s = "unren_py27.py"
-        # print("Target path missing. \
-        # Use: python3 unren_build.py '/path/to/target/script.py'")
-    tool2py(tools_pth, target_py3_s, target_py2_s)
+    tool2py(tools_pth, dst_py2, dst_py3)
 
-    # part2 - embed py files in the shell/cmd files
-    target_sh_s = "unren.sh"
-    target_cmd_s = "unren.cmd"
-    py2shell(target_py_s, target_sh_s, target_cmd_s)
+    # part3 - embed py files in the cmd file
+    # if cfg.make_cmd:
+    base_cmd = "unren_base.cmd"
+    dst_cmd2 = "unren27.cmd"
+    dst_cmd3 = "unren36.cmd"
+    py2cmd(embed_py2, embed_py3, base_cmd, dst_cmd2, dst_cmd3)
 
 
-    print("\nUnRen streambuilder:>> Task completed!\n")
+    print("\nUnRen builder:>> Task completed!\n")
 
 
 if __name__ == '__main__':
